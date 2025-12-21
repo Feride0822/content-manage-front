587936b08107cb27aeec6b38db9f9f1874113ac3
Kamalov-Q <kamolovquvomiddin9@gmail.com>
2025-12-21 13:00:07 +0500

    production ready

--- src/api/axios.js
+++ src/api/axios.js
@@ -15,7 +15,7 @@ api.interceptors.request.use((config) => {
   const token = localStorage.getItem(STORAGE_KEYS.TOKEN);
   if (token) {
     config.headers = config.headers || {};
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`; 
   }
   return config;
 });
